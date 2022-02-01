f9260ba71a6199b95ce801f9496dc348c9eb571e
Ido Melo <idomelo123@gmail.com>
2022-02-01 18:04:36 -0300

    adicionando estrutura inicial

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+//const path = require('path')
+
+const routes = require('./routes/routes')
+//const db = require('./database')
+
+const app = express()
+
+//conexão com banco de dados
+//db.connect()
+
+//Definindo server para receber dados via método post
+app.use(express.urlencoded({ extended: true }))
+
+//rotas
+app.use('/api', routes)
+
+//executando servidor
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log('listening on port ' + port))
