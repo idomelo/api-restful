30b385dfc22c5cf3ac53b0407fa908c8f44fd687
Ido Melo <idomelo123@gmail.com>
2022-02-02 18:35:29 -0300

    adicionando metodo delete

--- src/controller/products.js
+++ src/controller/products.js
@@ -52,8 +52,21 @@ async function put(req, res) {
   */
 }
 
+async function del(req, res) {
+  const { id } = req.params
+
+  const remove = await ProductsModel.deleteOne({_id: id})
+
+  const message = remove.deletedCount ? 'success' : 'error'
+
+  res.send({
+    message,
+  })
+}
+
 module.exports = {
   get,
   post,
-  put
+  put,
+  del
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,6 +5,7 @@ const ProductsController = require('../controller/products')
 router.get('/products/:id?', ProductsController.get)
 router.post('/products/', ProductsController.post)
 router.put('/products/:id', ProductsController.put)
+router.delete('/products/:id', ProductsController.del)
 
 
 module.exports = router
